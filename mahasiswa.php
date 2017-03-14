ba1e66e1a106af7da9ece92a427003577132782a
Faizal123 <dutalucu37@gmail.com>
2017-03-14 20:13:17 +0700

    Add files via upload

---
+++ mahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+$json_data=file_get_contents("mahasiswa.json");
+$obj = json_decode($json_data);
+echo "Nim : "; echo $obj->Nim;
+echo "<br>";
+echo "Nama : ";echo $obj->Nama;
+echo "<br>";
+echo "Alamat: ";echo $obj->Alamat;
+echo "<br>";
+echo "No_Hp: ";echo $obj->No_Hp;
+echo "<br>";
+echo "Mata_Kuliah: ";echo $obj->Mata_Kuliah[0];
+echo "<br>";
+echo "Mata_Kuliah: ";echo $obj->Mata_Kuliah[1];
+echo "<br>";
+echo "Mata_Kuliah: ";echo $obj->Mata_Kuliah[2];
+?>
+
+ 
